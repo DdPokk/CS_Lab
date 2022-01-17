3a2ced83d9fe8d20e9ac20d09c95394b94231156
Dmitry Yudin <DmitryYudin@itcenterifmo.onmicrosoft.com>
2022-01-17 19:49:50 +0300

    2.7

---
+++ Lab_2.7/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_2._7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Cheked == true)
+            {
+                Label lbl = new Label();
+                lbl.Location = new System.Drawing.Point(16, 96);
+                lbl.Size = new System.Drawing.Size(32, 23);
+                lbl.Name = "labelll";
+                lbl.TabIndex = 2;
+                lbl.Text = "PIN2";
+                groupBox1.Controls.Add(lbl);
+                TextBox txt = new TextBox();
+                txt.Location = new System.Drawing.Point(96, 96);
+                txt.Size = new System.Drawing.Size(184, 20);
+                txt.Name = "textboxx";
+                txt.TabIndex = 1;
+                txt.Text = "";
+                groupBox1.Controls.Add(txt);
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
